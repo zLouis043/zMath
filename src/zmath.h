44d744b2c843e92c9b1b815b479a9216d2b8e42f
zLouis043 <louis.003.lt@gmail.com>
2023-09-01 23:00:47 +0200

    added comments

--- src/zmath.h
+++ src/zmath.h
@@ -21,95 +21,177 @@ typedef struct zVec{
 
 const zVec NULL_VECTOR = {0, NULL};
 
-/*
+/*!
+    Prints the dimensions of the vector and its components.
 */
 void printV(zVec vector);
 
-/*
+/*!
+    Checks if the condition is true and printf an error message and exit the program if it is false.
+    @param condition The condition to check.
+    @param message The message to send if the condition is false.
+    @param filepath The path of the file where the assertion failed.
+    @param line The line in the file where the assertion failed.
 */
 void _zassert(bool condition, const char* message, const char* filepath, size_t line);
 
-/*
+/*!
+    Free the vector and sets its dimension to 0.
 */
 void freeZVector(zVec* vector);
 
-/*
+/*!
+    Allocate memory for the vector.
+    @param dim The dimension of the vector.
+    @result The memory allocated. 
 */
 zVec allocVector(size_t dim);
 
-/*
+/*!
+    Create a new vector of dim Dimension with every component in the vector equal to 0.
+    @param dim The dimension of the vector.
+    @result The new vector with every element in the vector equal to 0.
 */
 zVec newZeroVector(size_t dim);
 
-/*
+/*!
+    Create a new vector of dim Dimension with every component in the vector equal to to element.
+    @param dim The dimension of the vector.
+    @param element The element of which the vector will be filled.
+    @result The new vector with every component in the vector equal to element.
 */
 zVec newDefaultVector(size_t dim, float element);
 
-/*
+/*!
+    Create a new vector of dim Dimension.
+    @param dim The dimension of the vector.
+    @param ... The components of the vector.
+    @result The new vector of dim Dimension.
 */
 zVec _newZVector(size_t dim, ...);
 
-/*
+/*!
+    Copy the dimension and the components from a vector to another one.
+    @param vector The vector to copy.
+    @result The copy of the vector.
 */
 zVec copyVector(zVec vector);
 
-/*
+/*!
+    Sums the two vectors.
+    @param vector1
+    @param vector2
+    @result The sum between vector1 and vector2.
 */
 zVec sumVector(zVec vector1 , zVec vector2);
 
-/*
+/*!
+    Sums a vector and a scalar.
+    @param vector1
+    @param scalar
+    @result The sum between vector1 and a scalar.
 */
 zVec sumVectorByScalar(zVec vector1 , size_t scalar);
 
-/*
+/*!
+    Sums the two vectors.
+    @param vector1
+    @param vector2
+    @result The sum between vector1 and vector2.
 */
 zVec subVector(zVec vector1 , zVec vector2);
 
-/*
+/*!
+    Subtract a vector and a scalar.
+    @param vector1
+    @param scalar
+    @result The subtractation between vector1 and a scalar.
 */
 zVec subVectorByScalar(zVec vector1 , size_t scalar);
 
-/*
+/*!
+    Multiply two vectors.
+    @param vector1
+    @param vector2
+    @result The product between vector1 and  vector2.
 */
 zVec multVector(zVec vector1, zVec vector2);
 
-/*
+/*!
+    Multiply a vector and a scalar.
+    @param vector1
+    @param scalar
+    @result The product between vector1 and a scalar.
 */
 zVec multVectorByScalar(zVec vector1, size_t scalar);
 
-/*
+/*!
+    Divide two vectors.
+    @param vector1
+    @param vector2
+    @result The division between vector1 and  vector2.
 */
 zVec divVector(zVec vector1, zVec vector2);
 
-/*
+/*!
+    Divide a vector and a scalar.
+    @param vector1
+    @param scalar
+    @result The division between vector1 and a scalar.
 */
 zVec divVectorByScalar(zVec vector1, size_t scalar);
 
-/*
+/*!
+    Raise every component of a vector to the exponent given.
+    @param vector The vector to raise.
+    @param exponent The exponent of the power.
+    @result The vector raised to the exponent.
 */
 zVec powerVectorToExp(zVec vector, size_t exponent);
 
-/*
+/*!
+    The cross product between two vectors.
+    @param vector1
+    @param vector2
+    @result The cross product between two vectors.
 */
 zVec crossProduct(zVec vector1, zVec vector2);
 
-/*
+/*!
+    This function return the normalized version of a vector given.
+    @param vector The vector to normalize.
+    @result The normalized vector.
 */
 zVec normalizeVector(zVec vector);
 
-/*
+/*!
+    Checks if two vectors are equal.
+    @param vector1
+    @param vector2
+    @result true if they are equal false if otherwise.
 */
 bool areVecEqual(zVec vector1, zVec vector2);
 
-/*
+/*!
+    Checks if the two vectors are orthogonal.
+    @param vector1.
+    @param vector2.
+    @result true if they are orthogonal false if otherwise
 */
 bool areVecOrthogonal(zVec vector1, zVec vector2);
 
-/*
+/*!
+    This functions return the dot product of two vectors.
+    @param vector1
+    @param vector2
+    @result The dot product of the two vectors.
 */
 float dotProduct(zVec vector1, zVec vector2);
 
-/*
+/*!
+    This function return the magnitude of a vector.
+    @param vector @
+    @result The magnitude of the vector.
 */
 float magnitude(zVec vector);
 
@@ -119,23 +201,39 @@ float magnitude(zVec vector);
 #define EQUAL_ERROR "Dimension mismatch."
 #define ALLOC_ERROR "Allocation failure."
 
-/*
+/*!
+    Find the arguments given 
+    @param type The type of the arguments.
+    @param ... The variable arguments.
 */
 #define NUMARGS(type, ...) (sizeof((type[]){0.0f, ##__VA_ARGS__}) / sizeof(type) - 1) 
 
-/*
+/*!
+    Create a new vector of dim Dimension.
+    @param ... The components of the vector.
+    @result The new vector of dim Dimension.
 */
 #define newZVector(...) _newZVector(NUMARGS(float, ##__VA_ARGS__), __VA_ARGS__)
 
-/*
+/*!
+    Checks if the condition is true and printf an error message and exit the program if it is false.
+    @param condition The condition to check.
+    @param message The message to send if the condition is false.
 */
 #define zassert(condition, message) _zassert(condition, message, __FILE__, __LINE__)
 
-/*
+/*!
+    Return the value of the specified vector at the specified index.
+    @param vector The vector from which to retrieve the value.
+    @param index The index where to retrieve the value.
+    @result The value at the specified index.
 */
 #define ValueAt(vector, index) (vector.elements[index])
 
-/*
+/*!
+    Return the dimension of the specified vector.
+    @param vector The vector from which we pick the dimension.
+    @result The dimension of the vector.
 */
 #define DIM(vector) (vector.dim)
 
